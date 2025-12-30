f6bed551afc022ae5d7bae345600bb37458a66f2
marouana.diallo <marouanadiallo1@gmail.com>
2025-12-30 23:01:45 +0100

    validating users csv file

--- src/main/resources/ts/models.ts
+++ src/main/resources/ts/models.ts
@@ -1,3 +1,7 @@
+export function isKeyOf<T>(key: string, obj: T): key is keyof T {
+  return key in obj;
+}
+
 export declare type UserForm = {
     gender: "HOMME" | "FEMME" | null;
     firstName: string;
--- src/main/resources/ts/users-index.ts
+++ src/main/resources/ts/users-index.ts
@@ -1,4 +1,12 @@
-import {  UserForm } from "./models.ts";
+import {  UserForm, isKeyOf } from "./models.ts";
+import {
+    firstNameValidatorFn,
+    lastNameValidatorFn,
+    emailValidatorFn,
+    telephoneValidatorFn,
+    birthDateValidatorFn,
+    genderValidatorFn
+} from "./user-form-validators.ts";
 
 declare const Alpine: any;
 const fileTypes = ['text/csv', '.csv'];
@@ -22,6 +30,48 @@ function returnFileSize(size: number): string {
   return `${(size / 1e6).toFixed(1)} MB`;
 }
 
+function headerKeysAreValid(headers: Array<string>): boolean {
+  let valid = true;
+  headers.forEach(header => {
+    if(!isKeyOf<UserForm>(header, {
+        gender: null,
+        firstName: '',
+        lastName: '',
+        email: '',
+        telephone: '',
+        birthDate: '',
+    })) {
+            valid = false;
+        }
+    });
+    return valid;
+}
+
+function isValidColonneValue(position: number, value: string): any {
+    switch(position) {
+        case 0:
+           const genderError = genderValidatorFn(value as 'HOMME' | 'FEMME' | null);
+           return `genre invalide (${genderError ? null : 'seulement HOMME ou FEMME  est accepté'})`;
+        case 1:
+           const firstNameError = lastNameValidatorFn(value);
+           return firstNameError === '' ? null : `prénom invalide (${firstNameError})`;
+        case 2:
+           const lastNameError = firstNameValidatorFn(value);
+           return lastNameError === '' ? null : `nom invalide (${lastNameError})`;
+        case 3:
+           const emailError = emailValidatorFn(value);
+           return emailError === '' ? null : `email invalide (${emailError})`;
+        case 4:
+           const telephoneError = telephoneValidatorFn(value);
+           return telephoneError === '' ? null : `téléphone invalide (${telephoneError})`;
+        case 5:
+           const birthDateError = birthDateValidatorFn(value);
+           return birthDateError === '' ? null : `date de naissance invalide (${birthDateError})`;
+        default:
+           return null;
+    }
+}
+
 document.addEventListener('alpine:init', () => {
     Alpine.data('userIndexPage', () => ({
         loading: false,
@@ -29,12 +79,17 @@ document.addEventListener('alpine:init', () => {
         fileInput: null as HTMLInputElement | null,
         showPreviewModal: false,
         users: [] as Array<UserForm>,
+        errors: [] as Array<string>,
         
+        /**         
+         * This function resets all the data related to file upload and user preview.
+        */
         resetAll() {
             this.file = null;
             this.fileInput!.value = '';
             this.showPreviewModal = false;
             this.users = [];
+            this.errors = [];
         },
 
         /**
@@ -50,12 +105,12 @@ document.addEventListener('alpine:init', () => {
                 return;
             }
             if (!validFileType(this.file)) {
-                alert('Invalid file type. Please upload a CSV file.');
+                this.errors = ['Fichier non valide. Veuillez télécharger un fichier CSV.'];
                 fileInput.value = '';
                 return;
             }
             if (!validFileSize(this.file)) {
-                alert('File size exceeds the 10 MB limit.');
+                this.errors = ['La taille du fichier dépasse la limite de 10 Mo.'];
                 fileInput.value = '';
                 return;
             }
@@ -76,13 +131,42 @@ document.addEventListener('alpine:init', () => {
         parseCSV(text: string) {
             const lines = text.split('\n').filter(line => line.trim() !== '');
             const headers = lines[0].split(DELIMITER).map(h => h.trim());
-            this.users = lines.slice(1).map(line => {
+            
+            // Validate headers
+            if(!headerKeysAreValid(headers)) {
+                this.errors.push('Les en-têtes du fichier CSV ne correspondent pas aux champs attendus.');
+                this.errors.push('Champs attendus : gender, firstName, lastName, email, telephone, birthDate.');
+                return;
+            }
+
+            // Parse lines
+            this.users = [];
+            this.errors = [];
+            lines.slice(1).some((line, index) => {
                 const values = line.split(DELIMITER).map(v => v.trim());
+                if (values.length !== headers.length) {
+                    this.errors.push(`Nombre de colonnes incorrect à la ligne ${index + 2}.`);
+                    return true; // break the loop
+                }
+
                 const user: any = {};
-                headers.forEach((header, index) => {
-                    user[header] = values[index] || '';
+                let lineHasError = false;
+
+                headers.forEach((header, i) => {
+                   const errorMessage = isValidColonneValue(i, values[i]);
+                    if (errorMessage) {
+                        this.errors.push(`Ligne ${index + 2} : ${errorMessage}`);
+                        lineHasError = true;
+                    }
+                    user[header] = values[i];
                 });
-                return user as UserForm;
+                
+                if(lineHasError) {
+                    this.users = []; // clear users if any error in line
+                    return true; // break the loop
+                }
+                this.users.push(user as UserForm);
+                return false; // continue the loop
             });
         }
     }));
